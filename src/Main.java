22079353866ce10be13689cfebb88018e87d863f
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-03 08:36:53 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+                System.out.println("Wall Area!");
+
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
     }
 }
